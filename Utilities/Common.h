6f274eeed2ab3379cefbc844561c423832bc4a0b
John <johnbrochue@outlook.com>
2013-05-04 13:32:35 -0400

    Fixed some typedefs.

--- Utilities/Common.h
+++ Utilities/Common.h
@@ -12,6 +12,17 @@
 	#define threadlocal __thread
 #endif
 
+typedef long long int64;
+typedef int int32;
+typedef short int16;
+typedef char int8;
+typedef unsigned long long uint64;
+typedef unsigned int uint32;
+typedef unsigned short uint16;
+typedef unsigned char uint8;
+typedef double float64;
+typedef float float32;
+
 #ifdef WINDOWS
 
 #if defined _WIN64	
@@ -30,15 +41,5 @@ typedef uint32 word;
 
 #endif
 
-typedef long long int64;
-typedef int int32;
-typedef short int16;
-typedef char int8;
-typedef unsigned long long uint64;
-typedef unsigned int uint32;
-typedef unsigned short uint16;
-typedef unsigned char uint8;
-typedef double float64;
-typedef float float32;
 typedef const int8* cstr;
 typedef word uintptr;
