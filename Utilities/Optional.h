ea132724cce5df1f0438e47c74ddf4af0772fce7
John <johnbrochue@outlook.com>
2013-10-08 20:30:33 -0400

    Added an optional type.

---
+++ Utilities/Optional.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <utility>
+#include <type_traits>
+
+namespace util {
+	template<typename T> class optional {
+		T type;
+		bool is_set;
+
+		public:
+			optional() {
+				this->is_set = false;
+			}
+
+			optional(const T& obj) : type(obj) {
+				this->is_set = true;
+			}
+
+			optional(T&& obj) : type(std::move(obj)) {
+				this->is_set = true;
+			}
+
+			void operator=(const T& obj) {
+				this->type = obj;
+				this->is_set = true;
+			}
+
+			void operator=(T&& obj) {
+				this->type = std::move(obj);
+				this->is_set = true;
+			}
+
+			void set(const T& obj) {
+				this->type = obj;
+				this->is_set = true;
+			}
+
+			void set(T&& obj) {
+				this->type = std::move(obj);
+				this->is_set = true;
+			}
+
+			T value() {
+				return this->type;
+			}
+
+			T&& extract() {
+				this->is_set = false;
+				return std::move(this->type);
+			}
+
+			void clear() {
+				this->is_set = false;
+				this->type = T();
+			}
+
+			bool valid() const {
+				return this->is_set;
+			}
+
+			operator bool() const {
+				return this->is_set;
+			}
+
+			T& operator*() {
+				return this->type;
+			}
+
+			T& operator->() {
+				return this->type;
+			}
+	};
+
+	template<typename T> class optional<T&> {
+		std::remove_reference_t<T>* type;
+
+		public:
+			optional() {
+				this->type = nullptr;
+			}
+
+			optional(T& obj) : type(&obj) {
+
+			}
+
+			void operator=(T& obj) {
+				this->type = &obj;
+			}
+
+			void set(T& obj) {
+				this->type = &obj;
+			}
+
+			void clear() {
+				this->type = nullptr;
+			}
+
+			T& value() {
+				return *this->type;
+			}
+
+			bool valid() const {
+				return this->type != nullptr;
+			}
+
+			operator bool() const {
+				return this->type != nullptr;
+			}
+
+			T& operator*() {
+				return *this->type;
+			}
+
+			T& operator->() {
+				return *this->type;
+			}
+	};
+}
