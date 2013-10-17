7a9bd3129f5501a40050b316149a81d2fabe6dad
John <johnbrochue@outlook.com>
2013-10-17 19:37:42 -0400

    Added a Locked type. Currently uses a mutex, but will be refactored to store the lock in the upper byte of the pointer.

---
+++ Utilities/Locked.h
@@ -0,0 +1,33 @@
+#include <mutex>
+
+namespace util {
+	template<typename T> class locked {
+		std::mutex mtx;
+		T* data;
+
+		public:
+			locked(T* item = nullptr) {
+				this->data = item;
+			}
+
+			void lock() {
+				this->mtx.lock();
+			}
+
+			void unlock() {
+				this->mtx.unlock();
+			}
+
+			T* get() {
+				return this->data;
+			}
+
+			T& operator*() {
+				return *this->data;
+			}
+
+			T* operator->() {
+				return this->data;
+			}
+	};
+}
