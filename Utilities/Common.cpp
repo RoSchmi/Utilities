7b522827058e7c67c386b9972a1d30207471355a
John <johnbrochue@outlook.com>
2013-11-03 21:16:05 -0500

    Bug fix.

--- Utilities/Common.cpp
+++ Utilities/Common.cpp
@@ -5,7 +5,7 @@ using namespace std;
 date_time util::epoch;
 
 uint64 util::since_epoch(const date_time& dt) {
-	return chrono::duration_cast<chrono::milliseconds>(epoch - dt).count();
+	return chrono::duration_cast<chrono::milliseconds>(dt - epoch).count();
 }
 
 date_time util::from_epoch(uint64 val) {
