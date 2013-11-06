5ffff5ff4a74c062846605c619b07a266fb09804
Corey Richardson <corey@octayn.net>
2013-11-05 19:09:12 -0500

    Add make_unique for POSIX until libc++ 3.4 is released

--- Utilities/Common.h
+++ Utilities/Common.h
@@ -15,6 +15,34 @@
 
 #include <chrono>
 
+// libstdc++ doesn't support make_unique, libc++ supports it only in the
+// unreleased 3.4
+#ifdef POSIX
+
+#include <memory>
+#include <type_traits>
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique_helper(std::false_type, Args&&... args) {
+	  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique_helper(std::true_type, Args&&... args) {
+	   static_assert(std::extent<T>::value == 0,
+			          "make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+
+	      typedef typename std::remove_extent<T>::type U;
+		     return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
+}
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+	   return make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
+}
+
+#endif
+
 typedef long long int64;
 typedef int int32;
 typedef short int16;
